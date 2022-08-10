b95d405742af2a04c19127f70e0c704d60f5e533
Ege Hidiroglu <egehdrgl@gmail.com>
2022-08-10 12:09:45 -0400

    Setup the routes. Created controllers for review and restaurant. Created the index page.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,7 @@
 class Restaurant < ApplicationRecord
-#   A restaurant must have a name, an address and a category.
-# A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
-# When a restaurant is destroyed, all of its reviews must be destroyed as well.
-# A review must belong to a restaurant.
-# A review must have a content.
-# A review must have a rating.
-# A review’s rating must be a number between 0 and 5.
-# A review’s rating must be an integer. For example, a review with a rating of 2.5 should be invalid!
-  
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: %w[chinese italian japanese french belgian]
 end
